cba339dd3c3fdc1a36750ba8f61ebfe62d211501
Nicholas Young <hi@secretfader.com>
2026-01-12 14:23:48 -0700

    feat: update front page with new design and video

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import alpine from "@astrojs/alpinejs";
+
+const site = import.meta.env.CF_PAGES_URL || "https://fictiveflame.co";
 
 // https://astro.build/config
 export default defineConfig({
+  site,
   adapter: cloudflare(),
-
+  integrations: [sitemap(), alpine()],
   vite: {
     plugins: [tailwindcss()],
   },
